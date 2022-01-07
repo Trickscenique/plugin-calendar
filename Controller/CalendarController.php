1220c86d9006c6a19688ec12837c9ab6449080c6
Nassim Ourami <27701750+Trickscenique@users.noreply.github.com>
2022-01-07 20:46:38 +0100

    Pouf

--- Controller/CalendarController.php
+++ Controller/CalendarController.php
@@ -80,13 +80,13 @@ class CalendarController extends BaseController
                     $subtasks[$tmp_subtask['id']] = $this->subtaskModel->getAll($tmp_subtask['id']);
                 }
 
-                $subtask = array_filter($subtasks[$tmp_subtask['id']], function ($element, $k) use ($tmp_subtask) {
-                    return (t('#%d', $tmp_subtask['id']).' '.$element['title'] == $tmp_subtask['title']);
+                $subtask = array_filter($subtasks[$tmp_subtask['id']], function ($element, $k) use ($tmp_subtask, $parentTask) {
+                    return (t('#%d', $parentTask['id']).' '.$element['title'] == $tmp_subtask['title']);
                 }, ARRAY_FILTER_USE_BOTH);
 
                 $ref = "subtask";
                 $color = $parentTask['color_id'];
-                $events[$key]['id'] = $ref."-".$subtask['id'];
+                $events[$key]['id'] = $ref."-".$subtask[0]['id'];
                 $events[$key]['backgroundColor'] = $color;
                 $events[$key]['borderColor'] =  $color;
             }
